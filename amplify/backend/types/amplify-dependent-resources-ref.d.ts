1c9a93aef4e8ac2f66e02cf269ff64679cc94d81
Jules Ivan Vital <jimvital42@gmail.com>
2024-06-12 14:15:44 +0800

    feat: setup auth ui

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,22 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "pilikola2024": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "pilikola2024CustomMessage": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { Amplify } from "aws-amplify";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { Authenticator } from "@aws-amplify/ui-react";
 
 import { Layout } from "@/common";
+import awsExports from "@/aws-exports";
 
+import "@aws-amplify/ui-react/styles.css";
 import "./globals.css";
+
+Amplify.configure(awsExports);
 
 const darkTheme = createTheme({
   palette: {
@@ -21,12 +27,26 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <link rel="favicon" href="/public/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
+      <Authenticator
+        loginMechanisms={["email"]}
+        initialState="signIn"
+        signUpAttributes={[
+          "birthdate",
+          "email",
+          "name",
+          "nickname",
+          "preferred_username",
+        ]}
+      >
+        <Authenticator.Provider>
+          <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </ThemeProvider>
+        </Authenticator.Provider>
+      </Authenticator>
     </>
   );
 }
